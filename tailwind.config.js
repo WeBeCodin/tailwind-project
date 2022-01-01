363e88fd3c693f216682b03bc8c634e46bc407a5
WeBeCodin <93334652+WeBeCodin@users.noreply.github.com>
2022-01-01 23:51:01 +0800

    final tweeking

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,22 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
-    purge: [
-      './public/**/*.html',
-     './src/**/*.{js,jsx,ts,tsx,vue}',
-    ],
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
 
-    // add daisyUI plugin
+  // add daisyUI plugin
   plugins: [
     require('daisyui'),
     require('tailwindcss-scroll-snap'),
   ],
 
   variants: {
-		// Add variants as needed
-		scrollSnapType: ['responsive'],
-	},
-	
+    // Add variants as needed
+    scrollSnapType: ['responsive'],
+  },
+
   // config (optional)
   daisyui: {
     styled: true,
@@ -27,37 +27,37 @@ module.exports = {
     logs: true,
     rtl: false,
   },
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {
-        colors: {
-          coolGray: colors.coolGray,
-          gray: colors.gray,
-          lightBlue: colors.lightBlue,
-          blue: colors.blue,
-          blueGray: colors.blueGray,
-          red: colors.red,
-          rose: colors.rose,
-          pink: colors.fuchsia,
-          yellow: colors.yellow,
-          amber: colors.amber,
-          orange: colors.orange,
-          emerald: colors.emerald,
-          green: colors.green,
-          indigo: colors.indigo,
-          purple: colors.violet,
-          'regal-blue': '#243c5a',
-          'indigo-dark': '#202e78',
-          teal: colors.teal,
-          cyan: colors.cyan,
-          sky: colors.sky,
-          lime: colors.lime,
-        },
+  darkMode: true, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        coolGray: colors.coolGray,
+        gray: colors.gray,
+        lightBlue: colors.lightBlue,
+        blue: colors.blue,
+        blueGray: colors.blueGray,
+        red: colors.red,
+        rose: colors.rose,
+        pink: colors.fuchsia,
+        yellow: colors.yellow,
+        amber: colors.amber,
+        orange: colors.orange,
+        emerald: colors.emerald,
+        green: colors.green,
+        indigo: colors.indigo,
+        purple: colors.violet,
+        'regal-blue': '#243c5a',
+        'indigo-dark': '#202e78',
+        teal: colors.teal,
+        cyan: colors.cyan,
+        sky: colors.sky,
+        lime: colors.lime,
+      },
       screens: {
-        sm: '480px',
+        sm: '640px',
         md: '768px',
-        lg: '976px',
-        xl: '1440px',
+        lg: '1024px',
+        xl: '1280px',
       },
       fontFamily: {
         sans: ['Graphik', 'sans-serif'],
@@ -75,4 +75,3 @@ module.exports = {
     }
   }
 }
-  
